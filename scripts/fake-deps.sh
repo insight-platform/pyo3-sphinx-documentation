63de087b2f907a1e83057ddc6954f66673aa79aa
bwsw <bitworks@bw-sw.com>
2023-06-20 12:34:08 +0400

    added ci

--- scripts/fake-deps.sh
+++ scripts/fake-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 yum install -y epel-release
-yum install -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
-yum install -y --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+yum install -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+yum install -y --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 yum install -y ffmpeg ffmpeg-devel
